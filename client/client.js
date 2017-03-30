e52dc64099e901cf30ca4348d337801054b3d48e
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2017-03-31 00:48:45 +0300

    set up react

--- client/client.js
+++ client/client.js
@@ -1,1 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from '../components/App'
 
+render(
+  // define components
+  // DOM element to mount on
+  <App/>,
+  document.getElementById('app')
+)
