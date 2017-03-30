6a189abaa81c0053ad973336c558948d5c567b43
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2017-03-31 01:23:42 +0300

    set up hot reloading of components

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
-var express = require('express');
-var path = require('path');
+var express = require('express')
+var path = require('path')
+var app = express()
+var config = require('../webpack.config.js')
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var compiler = webpack(config)
+var port = 3000
 
-var app = express();
-
-app.use(express.static('./dist'));
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}))
+app.use(webpackHotMiddleware(compiler))
+app.use(express.static('./dist'))
 
 app.use('/', function (req, res) {
-    res.sendFile(path.resolve('client/index.html'));
-});
+  res.sendFile(path.resolve('client/index.html'))
+})
 
-var port = 3000;
-
-app.listen(port, function(error) {
-  if (error) throw error;
-  console.log("Express server listening on port", port);
-});
+app.listen(port, function (error) {
+  if (error) throw error
+  console.log('Express server listening on port', port)
+})
